4b02cccb507d59948c38dea5e6f07d5aa101bacc
TommyBlueSky <t07y31@gmail.com>
2024-10-17 12:19:15 +0900

    pudate:port

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json()); // JSONの受信を許可
 // MySQLの接続設定（本番公開時には.envファイルに書き換える）
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
@@ -71,6 +70,7 @@ app.delete('/messages/:id', (req, res) => {
 });
 
 // サーバーを起動
-app.listen(process.env.DB_PORT || 5000, () => {
-  console.log(`Server is running`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
